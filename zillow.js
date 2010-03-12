13c1cbd73ce51ccaf7c7a5ed7fbadfd19f362a43
Ryan Sonnek <ryan@socialcast.com>
2010-03-11 20:50:59 -0600

    integrate with zillow api

--- zillow.js
+++ zillow.js
@@ -1,7 +1,14 @@
+function isValidZipCode(value) {
+  var re = /^\d{5}([\-]\d{4})?$/;
+  return (re.test(value));
+}
+
 (function($) {
-  $(function() {
-    var geocoder = new google.maps.Geocoder();
-    geocoder.geocode({'address': '4611 zane ave north crystal, mn'}, function(results, status) {
+  var geocoder = new google.maps.Geocoder();
+
+  zillow = {};
+  zillow.lookup = function(address) {
+    geocoder.geocode({'address': address}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
         var result = results[0]
         var address = result.formatted_address;
@@ -18,55 +25,35 @@
           title: address
         });
         $('#external_map').attr('href', 'http://maps.google.com?near=' + address);
+        
+        var street_address = address.split(',')[0];
+        var zip = null;
+        $.each(result.address_components, function() {
+          if (isValidZipCode(this.short_name) && isValidZipCode(this.long_name)) {
+            zip = this.short_name;
+          }
+        });
+        var data = {
+          'zws-id': 'X1-ZWz1csm2cyipsb_6plsv', 
+          'address': street_address,
+          'citystatezip': zip
+        };
+        $.get('http://www.zillow.com/webservice/GetSearchResults.htm', data, function(xml) {
+          var result = $(xml).find('result');
+          var zestimate = result.find('zestimate');
+
+          $('#estimate').text(zestimate.find('amount').text());
+          var valuation = zestimate.find('valuationRange');
+          $('#estimate_low').text(valuation.find('low').text());
+          $('#estimate_high').text(valuation.find('high').text());
+        });
       }
     });
-
+  };
+  $(function() {
     $('#form').submit(function() {
-
+      zillow.lookup($('#address').val());
       return false;
     });
   });
 })(jQuery);
-
-// CmdUtils.CreateCommand({
-//   name: "zillow",
-//   icon: "http://www.zillow.com/favicon.ico",
-//   homepage: "http://ryan.codecrate.com",
-//   author: { name: "Ryan Sonnek", email: "ryan@codecrate.com"},
-//   license: "MIT",
-//   description: "Search for property value on zillow.com",
-//   help: "select an address and invoke this command",
-//   takes: {"address": noun_arb_text},
-//   searchUrl: 'http://www.zillow.com/webservice/GetSearchResults.htm',
-//   postData: function(input) {
-//     var address = input.text;
-//     var zipcode = address.substring(address.lastIndexOf(' '));
-//     var street = address.substring(0, address.lastIndexOf(' '));
-//     return {'zws-id': 'X1-ZWz1csm2cyipsb_6plsv', 'address': address, 'citystatezip': zipcode};
-//   },
-//   preview: function(pblock, address) {
-//     pblock.innerHTML = 'Loading...';
-//     
-//     jQuery.get(this.searchUrl, this.postData(address), function(xml) {
-//       var result = jQuery(xml).find('result');
-// 
-//       var html = '';
-//       html += result.size() + ' result found. <br />';
-// 
-//       html += "<p>" + address.text + "</p>";
-//       
-//       html += "<b>$" + result.find('zestimate').find('amount').text() + "</b><br />";
-//       html += "<i>($" + result.find('zestimate').find('valuationRange').find('low').text() + " - ";
-//       html += "$" + result.find('zestimate').find('valuationRange').find('high').text() + ")</i>";
-// 
-//       pblock.innerHTML = html;
-//     });
-//   },
-//   execute: function(address) {
-//     jQuery.get(this.searchUrl, this.postData(address), function(xml) {
-//       var result = jQuery(xml).find('result');
-//       var url = result.find('links').find('homedetails').text();
-//       Utils.openUrlInBrowser(url);
-//     });
-//   }
-// });
