5143c30ef7f1088ed5cd25953c1f13679b27c14c
Ryan Sonnek <ryan@socialcast.com>
2010-03-12 08:52:32 -0600

    refactor and extract helper methods

--- zillow.js
+++ zillow.js
@@ -6,7 +6,11 @@ function isValidZipCode(value) {
 (function($) {
   var geocoder = null;
   zillow = {};
-  zillow.lookup = function(address) {
+  zillow.api_key = 'X1-ZWz1csm2cyipsb_6plsv';
+  zillow.findAddress = function(address, callback) {
+    $('#map_area').hide();
+    
+    if (geocoder == null) { geocoder = new google.maps.Geocoder(); }
     geocoder.geocode({'address': address}, function(results, status) {
       if (status != google.maps.GeocoderStatus.OK) {
         $('#errors').show().text('Unable to locate address');
@@ -35,6 +39,15 @@ function isValidZipCode(value) {
         return false;
       });
       
+      callback(result);
+    });
+  };
+  zillow.findEstimate = function(address) {
+    $('#errors').hide();
+    $('#zestimate').hide();
+    
+    zillow.findAddress(address, function(result) {
+      var address = result.formatted_address;
       var street_address = address.split(',')[0];
       var zip = null;
       $.each(result.address_components, function() {
@@ -43,38 +56,43 @@ function isValidZipCode(value) {
         }
       });
       var data = {
-        'zws-id': 'X1-ZWz1csm2cyipsb_6plsv', 
+        'zws-id': zillow.api_key, 
         'address': street_address,
         'citystatezip': zip
       };
-      $.get('http://www.zillow.com/webservice/GetSearchResults.htm', data, function(xml) {
-        if ($(xml).find('message code').text() != '0') {
-          $('#errors').show().text($(xml).find('message text').text());
-          return;
-        }
-        $('#errors').hide();
-        $('#zestimate').show();
-        $('#home_details').click(function() {
-          chrome.tabs.create({
-            url: $(xml).find('links homedetails').text()
+      $.ajax({
+        url: 'http://www.zillow.com/webservice/GetSearchResults.htm',
+        data: data,
+        dataType: 'xml',
+        type: 'GET',
+        success: function(xml) {
+          if ($(xml).find('message code').text() != '0') {
+            $('#errors').show().text($(xml).find('message text').text());
+            return;
+          }
+          $('#zestimate').show();
+          $('#home_details').click(function() {
+            chrome.tabs.create({
+              url: $(xml).find('links homedetails').text()
+            });
           });
-        });
 
-        var result = $(xml).find('result');
-        var zestimate = result.find('zestimate');
+          var result = $(xml).find('result');
+          var zestimate = result.find('zestimate');
 
-        $('#estimate').text(zestimate.find('amount').text());
-        var valuation = zestimate.find('valuationRange');
-        $('#estimate_low').text(valuation.find('low').text());
-        $('#estimate_high').text(valuation.find('high').text());
+          $('#estimate').text(zestimate.find('amount').text());
+          var valuation = zestimate.find('valuationRange');
+          $('#estimate_low').text(valuation.find('low').text());
+          $('#estimate_high').text(valuation.find('high').text());
+        }
       });
     });
   };
+
   $(function() {
     $('#form').submit(function() {
-      zillow.lookup($('#address').val());
+      zillow.findEstimate($('#address').val());
       return false;
     });
-    geocoder = new google.maps.Geocoder();
   });
 })(jQuery);
